807c424897a3d5105b09e885e0d1fa98a4db1073
Mustapha Benfetta <mustapha.benfetta@gmail.com>
2026-02-09 22:29:20 +0100

    Updated bottom-nav.component.ts

--- src/app/shared/bottom-nav.component.ts
+++ src/app/shared/bottom-nav.component.ts
@@ -124,7 +124,7 @@ import { AuthService } from '../core/auth.service';
 
     .nav-label {
       font-size: 10px;
-      font-weight: 600;
+      font-weight: 500;
       text-align: center;
       line-height: 1.1;
     }
